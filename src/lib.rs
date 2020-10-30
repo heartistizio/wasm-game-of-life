bbee6e303673df9f50828fb2a673a6f8b3146c53
Aleksander Kuźma <aleksander.kuzma@schibsted.com>
2020-10-30 12:46:04 +0100

    feat: initial version of game of life

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,7 @@
-mod utils;
-
-use wasm_bindgen::prelude::*;
+mod universe;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello {}!", name));
-}
